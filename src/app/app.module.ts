7d8c2ee426c9885d7c941582bda8a7fc655f384a
AyelenIasich <ayeleniasich@gmail.com>
2022-07-03 19:00:02 -0300

    Se incorpora la carpeta servicios con el servicio Portfolio para poder obtener en el futuro los datos de un API. Se agrega en la carpeta assets el archivo data.json con los datos temporales para usar de prueba.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { ReactiveFormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
+import {HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
@@ -12,7 +14,9 @@ import { HeaderComponent } from './components/header/header.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    ReactiveFormsModule,
+    AppRoutingModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
